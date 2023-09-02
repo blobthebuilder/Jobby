39a85992ccb8c9566aa0d8d4045fe00d3252ec3a
Frank Wang <frankningwang@gmail.com>
2023-09-02 15:27:32 -0700

    added map and location features

---
+++ frontend2/src/components/Navbar.js
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
+
+const Navbar = () => {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  const handleClick = () => {
+    logout();
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>Title</h1>
+        </Link>
+        <nav>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Logout</button>
+              <Link to="/jobboard">All Jobs</Link>
+              <Link to="/">Home</Link>
+              <Link to="/map">Map</Link>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ frontend2/src/pages/Map.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+} from "@react-google-maps/api";
+
+function Map() {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    libraries: ["places"],
+  });
+
+  const [lat, setLat] = useState(48.85);
+  const [lng, setLong] = useState(2.29);
+  const successCallback = (position) => {
+    setLat(position.coords.latitude);
+    setLong(position.coords.longitude);
+  };
+
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+
+  if (!isLoaded) {
+    return <p>Is Loading</p>;
+  }
+  const center = { lat: lat, lng: lng };
+  return (
+    <div>
+      <GoogleMap
+        center={center}
+        zoom={15}
+        mapContainerStyle={{ width: "100%", height: "100vh" }}
+        options={{
+          streetViewControl: false,
+          mapTypeControl: false,
+          fullscreenControl: false,
+          zoomControl: true,
+        }}
+        onLoad={(map) => setMap(map)}>
+        <Marker position={center} />
+      </GoogleMap>
+      <button
+        text="center"
+        onClick={() => map.panTo(center)}
+      />
+      <Autocomplete>
+        <input
+          type="text"
+          placeholder="destination"
+        />
+      </Autocomplete>
+    </div>
+  );
+}
+
+export default Map;
